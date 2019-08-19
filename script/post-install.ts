6925f22e4da4838d955ffa848d916f5c2a12fe74
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2019-08-19 14:41:13 +0200

    remove yarn

--- script/post-install.ts
+++ script/post-install.ts
@@ -1,5 +1,4 @@
 import { spawnSync, SpawnSyncOptions } from "child_process";
-import * as glob from "glob";
 import * as path from "path";
 
 const root = path.dirname(__dirname);
@@ -9,28 +8,12 @@ const options: SpawnSyncOptions = {
   stdio: "inherit"
 };
 
-function findYarnVersion(callback: (path: string) => void) {
-  glob("vendor/yarn-*.js", (error, files) => {
-    if (error != null) {
-      throw error;
-    }
+const result = spawnSync(
+  "yarn",
+  ["--cwd", "app", "install", "--force"],
+  options
+);
 
-    files.sort();
-
-    const latestVersion = files[files.length - 1];
-
-    callback(latestVersion);
-  });
+if (result.status !== 0) {
+  process.exit(result.status!);
 }
-
-findYarnVersion(path => {
-  let result = spawnSync(
-    "node",
-    [path, "--cwd", "app", "install", "--force"],
-    options
-  );
-
-  if (result.status !== 0) {
-    process.exit(result.status!);
-  }
-});
