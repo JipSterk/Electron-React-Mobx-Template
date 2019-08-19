4d602ca712b4d64b76f4ab57831122b724ec4c0e
Jip Sterk <17934897+JipSterk@users.noreply.github.com>
2019-08-19 14:31:27 +0200

    add postinstall script

---
+++ script/post-install.ts
@@ -0,0 +1,36 @@
+import { spawnSync, SpawnSyncOptions } from "child_process";
+import * as glob from "glob";
+import * as path from "path";
+
+const root = path.dirname(__dirname);
+
+const options: SpawnSyncOptions = {
+  cwd: root,
+  stdio: "inherit"
+};
+
+function findYarnVersion(callback: (path: string) => void) {
+  glob("vendor/yarn-*.js", (error, files) => {
+    if (error != null) {
+      throw error;
+    }
+
+    files.sort();
+
+    const latestVersion = files[files.length - 1];
+
+    callback(latestVersion);
+  });
+}
+
+findYarnVersion(path => {
+  let result = spawnSync(
+    "node",
+    [path, "--cwd", "app", "install", "--force"],
+    options
+  );
+
+  if (result.status !== 0) {
+    process.exit(result.status);
+  }
+});
